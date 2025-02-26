34cc7a55f9663363b32708a410ef7d4f4e55ea58
Vapotank <84374610+Vapotank@users.noreply.github.com>
2025-02-26 21:22:22 +0100

    Update install_ansible.sh

--- ANCSIBLE/install_ansible.sh
+++ ANCSIBLE/install_ansible.sh
@@ -21,23 +21,30 @@ apt install -y ansible
 echo "Détection des interfaces réseau actives :"
 # Récupérer la liste des interfaces actives possédant une adresse IPv4
 interfaces=($(ip -o -4 addr show | awk '{print $2}' | sort | uniq))
+declare -A iface_ips
+for iface in "${interfaces[@]}"; do
+  ip_addr=$(ip -o -4 addr show "$iface" | awk '{print $4}' | cut -d/ -f1)
+  iface_ips["$iface"]=$ip_addr
+done
+
 if [ ${#interfaces[@]} -gt 1 ]; then
   echo "Plusieurs interfaces détectées :"
-  select iface in "${interfaces[@]}"; do
-    if [ -n "$iface" ]; then
-      INTERFACE=$iface
-      break
-    else
-      echo "Sélection invalide, veuillez réessayer."
-    fi
+  for i in "${!interfaces[@]}"; do
+    echo "$((i+1))) ${interfaces[$i]} : ${iface_ips[${interfaces[$i]}]}"
   done
+  read -p "Choisissez une interface (1-${#interfaces[@]}) : " choice
+  if ! [[ "$choice" =~ ^[0-9]+$ ]] || [ "$choice" -lt 1 ] || [ "$choice" -gt ${#interfaces[@]} ]; then
+    echo "Choix invalide. Arrêt du script."
+    exit 1
+  fi
+  INTERFACE=${interfaces[$((choice-1))]}
 else
   INTERFACE=${interfaces[0]}
-  echo "Interface unique détectée : $INTERFACE"
+  echo "Interface unique détectée : $INTERFACE : ${iface_ips[$INTERFACE]}"
 fi
 
-# Récupérer l'adresse IP associée à l'interface sélectionnée
-IP=$(ip -o -4 addr show "$INTERFACE" | awk '{print $4}' | cut -d/ -f1)
+# Récupérer l'adresse IP de l'interface sélectionnée
+IP=${iface_ips[$INTERFACE]}
 echo "Interface sélectionnée : $INTERFACE"
 echo "Adresse IP détectée : $IP"
 
